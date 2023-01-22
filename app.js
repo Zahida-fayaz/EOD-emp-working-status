6edce6ff30a62f86e8e4b34897be9207a4cef303
shahzahida <shahzahida643@gmail.com>
2023-01-22 15:50:58 +0530

    created an index page and modified app.js

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ mongoose.connect("mongodb+srv://zahida:zahida@test.kgff3hf.mongodb.net/test")
 });   
 
 
-app.get("/", function(req, res){
+app.set("view engine", "ejs");
+
+app.get("/index", function(req, res){
     res.render("index");
 });
 
@@ -21,6 +23,6 @@ app.get("/", function(req, res){
 
 
 
-app.listen("3000", function(){
+app.listen("8080", function(){
     console.log("our web app is working on 3000 port number");
 });
