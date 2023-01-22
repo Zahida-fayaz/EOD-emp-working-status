f3bca9619ede7ae9a863feebaf308d1275ec74af
shahzahida <shahzahida643@gmail.com>
2023-01-22 12:51:48 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://zahida:zahida@test.kgff3hf.mongodb.net/test")
+.then(() => {
+    console.log("Database Connected");
+})
+.catch((err) => {
+    console.log("you did something wrong, database not connected");
+    console.log(err);
+});   
 
 
 app.get("/", function(req, res){
@@ -12,4 +23,4 @@ app.get("/", function(req, res){
 
 app.listen("3000", function(){
     console.log("our web app is working on 3000 port number");
-})
+});
