fad64d5a113f5bf114914e4b477c204158f55706
shahzahida <shahzahida643@gmail.com>
2023-01-26 11:20:11 +0530

    modified routes and lists

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const reportController = require('../controllers/reportController');
 
 //all reports
-router.get('/reports', reportController.all_reports);
+router.get('/', reportController.all_reports);
 
 //to new page
-router.get('/reports/new', reportController.reports_new);
+router.get('/new', reportController.reports_new);
 
 //add data
-router.post('/reports/new', reportController.reports_new_post);
+router.post('/new', reportController.reports_new_post);
 
 //show one
-router.get('/reports/:id',reportController.report_id);
+router.get('/:id',reportController.report_id);
 
 //to edit page
-router.get('/reports/:id/edit',reportController.reports_id_edit);
+router.get('/:id/edit',reportController.reports_id_edit);
 
 //edit data and post it
-router.post('/reports/:id/update',reportController.reports_id_update);
+router.post('/:id/update',reportController.reports_id_update);
 
 //delte route
-router.get('/reports/:id/delete',reportController.reports_id_delete);
+router.get('/:id/delete',reportController.reports_id_delete);
 
 module.exports = router
