3ef0b957ec40fb888bac69c812c378928eb8cc8c
shahzahida <shahzahida643@gmail.com>
2023-01-29 18:56:02 +0530

    created route for login page and modified it

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,20 @@
  const Regiister = require('../models/user');
- /*
+ 
  const bcrypt = require('bcryptjs');
  const jwt = require('jsonwebtoken');
-*/
+
 //gt reg page
 const register_new_user = (req, res) => {
     res.render('components/signup')
 };
 
 //register
-const register_new_user_post = (req, res) => {
+const register_new_user_post = (req, res) => { 
+   /* bcrypt.hash(req.body.password, 10, function( error, hashedPassword){
+    if(error){
+        console.log("error occured while encrypting password");
+    }*/
+    
     const regiister = new Regiister(req.body);
     regiister.save()
         .then(() => {
@@ -21,9 +26,16 @@ const register_new_user_post = (req, res) => {
             console.log(err)
         })
     //res.redirect('/show');
+//}); 
 };
+
+const login_new_user = (req, res) => {
+    res.render('components/login')
+};
+
 
 module.exports = {
     register_new_user,
-    register_new_user_post
+    register_new_user_post,
+    login_new_user 
 } 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,6 @@ router.get('/signup', authController.register_new_user);
 //add user info
 router.post('/signup', authController.register_new_user_post);
 
+router.get('/login', authController.login_new_user);
+
 module.exports = router
